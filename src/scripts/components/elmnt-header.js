c5b164f735dda2bef3c7aefdefd71783e0263444
darektoa <darektoa@gmail.com>
2020-08-15 18:41:22 +0700

    Add Animation On elmnt-header

--- src/scripts/components/elmnt-header.js
+++ src/scripts/components/elmnt-header.js
@@ -1,3 +1,6 @@
+import $ from '/src/scripts/manipulation.js';
+
+let prevScrollY = 0;
 class ElmntHeader extends HTMLElement{
     connectedCallback(){
         this.render();
@@ -12,7 +15,8 @@ class ElmntHeader extends HTMLElement{
     render(){
         this.innerHTML = `
             <a href="/">
-                <img src="src/images/wartech/transparent.png" alt="Wartech">
+                <img src="src/images/wartech/72x72.png" alt="Wartech">
+                Wartech
             </a>
                 
             <ul>
@@ -20,6 +24,31 @@ class ElmntHeader extends HTMLElement{
                 <li>Menu</li>
             </ul>
         `
+
+        window.addEventListener('scroll', this.onScroll);
+    }
+
+    // METHODE ANIMATION ON SCROLL
+    onScroll(){
+        const elmnt     = $('elmnt-header');
+        const width     = window.innerWidth;
+        const scrollY   = window.pageYOffset;
+
+        if((width >= 1000 && scrollY >= 700 && prevScrollY < scrollY) || (width < 1000 && scrollY >= 500 && prevScrollY < scrollY)){
+            elmnt.style = `
+                position: fixed;
+                top: 0;
+            `;
+        }else if(scrollY >= 10){
+            elmnt.style = `
+                top: -100px;
+                position: fixed;
+            `;
+        }else{
+           elmnt.removeAttribute('style');
+        }
+
+        prevScrollY = scrollY;
     }
 }
 
